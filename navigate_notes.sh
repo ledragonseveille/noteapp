c64a9d6a363227bcc03dc9060a40a88862ef4e19
ledragonseveille <ledragonseveille@users.noreply.github.com>
2025-02-13 10:49:21 +0100

    Translate navigate_notes.sh in english

--- navigate_notes.sh
+++ navigate_notes.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# Répertoire des fichiers HTML
+# HTML file directory
 HTML_DIR="$HOME/notes_app/html"
 
-# Lister les fichiers HTML disponibles
+# List available HTML files
 html_files=($(ls "$HTML_DIR"))
 
-# Afficher les fichiers HTML et demander à l'utilisateur de sélectionner celui à ouvrir
-echo "Sélectionnez la note à ouvrir:"
+# View HTML files and ask the user to select which one to open
+echo "Select the note you want to open:"
 select html_file in "${html_files[@]}"; do
     if [[ -n "$html_file" ]]; then
         xdg-open "$HTML_DIR/$html_file"
-        echo "Ouverture de '$html_file'"
+        echo "Opening of '$html_file'"
     else
-        echo "Sélection non valide"
+        echo "Invalid selection"
     fi
 done
